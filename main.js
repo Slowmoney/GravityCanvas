ea069ee214f054b29b00c84e868460f4be386788
Slowmoney <frolenkov58@gmail.com>
2020-03-31 23:24:13 +0300

    Update main.js

--- main.js
+++ main.js
@@ -126,28 +126,28 @@ window.countFPS = (function () {
     var lastLoop = (new Date()).getMilliseconds();
     var count = 1;
     var fps = 0;
-  
+
     return function () {
-      var currentLoop = (new Date()).getMilliseconds();
-      if (lastLoop > currentLoop) {
-        fps = count;
-        count = 1;
-      } else {
-        count += 1;
-      }
-      lastLoop = currentLoop;
-      return fps;
+        var currentLoop = (new Date()).getMilliseconds();
+        if (lastLoop > currentLoop) {
+            fps = count;
+            count = 1;
+        } else {
+            count += 1;
+        }
+        lastLoop = currentLoop;
+        return fps;
     };
-  }());
-  
- 
+}());
+
+
 var T = 0.1;
 var objectsToDraw = [0, 0, 0, 0];
 
 function update(dt) {
- 
-    document.querySelector(".fps").innerText =  countFPS();
-   
+
+    document.querySelector(".fps").innerText = countFPS();
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     if (objectsToDraw[0] != 0) {
 
@@ -166,7 +166,7 @@ function update(dt) {
     }
     checkCollision(window.obj);
     if (anim) {
-       requestAnimationFrame(update);
+        requestAnimationFrame(update);
     }
 
 }
@@ -197,11 +197,11 @@ function updatePos(dt) {
     obj[1].y += dt * obj[1].vy
 
 
-   
+
 }
 
 function checkCollision(o) {
-    for (let [i, o1] of o.entries()) {  
+    for (let [i, o1] of o.entries()) {
 
         for (let [j, o2] of o.entries()) {
             if (i < j) {
@@ -210,37 +210,64 @@ function checkCollision(o) {
                 let d = Math.sqrt(dx ** 2 + dy ** 2);
                 if (d < o1.r + o2.r) {
 
-                   
+
                     let impulse1x = o1.m * o1.vx;
-                    let impulse1y =o1.m * o1.vy;
-                    let impulse2x  =o2.m * o2.vx;
-                    let impulse2y =o2.m * o2.vy;
+                    let impulse1y = o1.m * o1.vy;
+                    let impulse2x = o2.m * o2.vx;
+                    let impulse2y = o2.m * o2.vy;
 
                     let dimpx = impulse1x + impulse2x;
                     let dimpy = impulse1y + impulse2y;
-                    let newvx = dimpx/(o1.m+o2.m);
-                    let newvy = dimpy/(o1.m+o2.m);
-                    console.log(newvx);
-                    console.log(newvy);
+                    let newvx = dimpx / (o1.m + o2.m);
+                    let newvy = dimpy / (o1.m + o2.m);
+                 //   console.log(newvx);
+                 //   console.log(newvy);
+
+
+                    let fi = Math.PI;
+                    let teta1 = Math.atan(o1.vx/o1.vy);// угол движения
+                    let teta2 = Math.atan(o2.vx/o2.vy);///
+                   // console.log(teta1*180/Math.PI);
+                   // console.log(teta2*180/Math.PI);
+                    let v1x = o1.vx;
+                    let v2x = o2.vx;
+                    let v1y = o1.vy;
+                    let v2y = o2.vy;
+
+                    let dm = o1.m + o2.m;
+                    let m1 = o1.m;
+                    let m2 = o2.m;
+
+                    let v1sx = ((v1x * Math.cos(teta1 - fi) * (dm) + 2 * m2 * v2x * Math.cos(teta2 - fi)) / (dm)) * Math.cos(fi) + v1x * Math.sin(teta1 - fi) * Math.cos(fi + Math.PI / 2);
+                    let v1sy = ((v1y * Math.cos(teta1 - fi) * (dm) + 2 * m2 * v2y * Math.cos(teta2 - fi)) / (dm)) * Math.sin(fi) + v1y * Math.sin(teta1 - fi) * Math.sin(fi + Math.PI / 2);
+
+
+                    let v2sx = ((v2x * Math.cos(teta1 - fi) * (dm) + 2 * m1 * v1x * Math.cos(teta2 - fi)) / (dm)) * Math.cos(fi) + v2x * Math.sin(teta1 - fi) * Math.cos(fi + Math.PI / 2);
+                    let v2sy = ((v2y * Math.cos(teta1 - fi) * (dm) + 2 * m1 * v1y * Math.cos(teta2 - fi)) / (dm)) * Math.sin(fi) + v2y * Math.sin(teta1 - fi) * Math.sin(fi + Math.PI / 2);
+                    console.log(v1sx, v1sy);
+
+
+
+
+
                     if (o1.m > o2.m) {
 
 
-
-                        o1.vx = newvx;
-                        o1.vy = newvy;
-                       
-                        o1.m += o2.m;
-                        o1.r = o1.m;
+                          o1.vx = v1sx;
+                          o1.vy = v1sy;
+                     
+                      //  o1.m += o2.m;
+                     //   o1.r = o1.m;
 
                         o.splice(j, 1);
                     } else {
-                       
-                        o2.vx = newvx;
-                        o2.vy = newvy;
+
+                        o2.vx = v2sx;
+                        o2.vy = v2sy;
 
 
-                        o2.m += o1.m;
-                        o2.r = o2.m;
+                      //  o2.m += o1.m;
+                      //  o2.r = o2.m;
 
                         o.splice(i, 1);
                     }
@@ -312,39 +339,39 @@ class Shape {
 
 
 }
-function moveWithGravity(dt, o) { 
+function moveWithGravity(dt, o) {
 
-    for (let o1 of o) {  
+    for (let o1 of o) {
         o1.fx = 0;
         o1.fy = 0;
     }
-    for (let [i, o1] of o.entries()) {  
+    for (let [i, o1] of o.entries()) {
         for (let [j, o2] of o.entries()) {
-            if (i < j) { 
-                let dx = o2.x - o1.x; 
+            if (i < j) {
+                let dx = o2.x - o1.x;
                 let dy = o2.y - o1.y;
                 let r = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
-                if (r < 1) {  
+                if (r < 1) {
                     r = 1;
                 }
-                let f = (1000 * o1.m * o2.m) / Math.pow(r, 2);  
-                let fx = f * dx / r; 
+                let f = (1000 * o1.m * o2.m) / Math.pow(r, 2);
+                let fx = f * dx / r;
                 let fy = f * dy / r;
-                o1.fx += fx;  
+                o1.fx += fx;
                 o1.fy += fy;
-                o2.fx -= fx;  
+                o2.fx -= fx;
                 o2.fy -= fy;
             }
         }
     }
-    for (let o1 of o) {  
-        let ax = o1.fx / o1.m; 
+    for (let o1 of o) {
+        let ax = o1.fx / o1.m;
         let ay = o1.fy / o1.m;
 
-        o1.vx += ax * dt; 
+        o1.vx += ax * dt;
         o1.vy += ay * dt;
 
-        o1.x += o1.vx * dt; 
+        o1.x += o1.vx * dt;
         o1.y += o1.vy * dt;
     }
 
